62b17868822e7d4456192f90c8c70bfbffb00b20
Tim <81175852+wharvex@users.noreply.github.com>
2025-02-10 19:41:03 -0500

    first commit

---
+++ src/main/java/com/wharvex/softwarevaluetracker/SoftwarevaluetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.wharvex.softwarevaluetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwarevaluetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwarevaluetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wharvex/softwarevaluetracker/SoftwarevaluetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wharvex.softwarevaluetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwarevaluetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
