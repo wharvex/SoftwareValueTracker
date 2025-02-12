2505137c5ffbeca6bcec6c49d8b806a7ab5fe12e
Tim <81175852+wharvex@users.noreply.github.com>
2025-02-11 19:57:06 -0500

    create app entity

---
+++ src/main/java/com/wharvex/softwarevaluetracker/App.java
@@ -0,0 +1,37 @@
+package com.wharvex.softwarevaluetracker;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class App {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String appName;
+
+    // Exists only for the sake of JPA.
+    protected App() {
+    }
+
+    public App(String appName) {
+	this.appName = appName;
+    }
+
+    public Long getId() {
+	return id;
+    }
+
+    public String getAppName() {
+	return appName;
+    }
+
+    @Override
+    public String toString() {
+	return String.format("App[id=%d, appName='%s']", id, appName);
+    }
+
+}
